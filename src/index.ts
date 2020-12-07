9f114799f94534d219fbc7eb34203d6966e06fbc
alexlee-dev <alex@alexlee.dev>
2020-12-07 10:24:02 -0800

    ✏️ Small Refactor to Overall Main Logic

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,64 @@ import getDay from "date-fns/getDay";
 
 import { Article, DayGroup } from "./types";
 
+const getArticleData = async (days: number): Promise<Article[] | void> => {
+  return new Promise((resolve, reject) => {
+    try {
+      fetch(`https://dev.to/api/articles?top=${days}&per_page=100`, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data: Article[]) => {
+          resolve(data);
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+    } catch (error) {
+      console.error(error);
+      reject(error);
+    }
+  });
+};
+
+const analyzeArticles = (articles: Article[], dayGroups: DayGroup[]) => {
+  // * Sort into days of the week
+  // * Add sum of `postitve_reactions_count`
+  articles.forEach((article) => {
+    // * Number 0 - 6, 0 being Sunday
+    const publishDay = getDay(new Date(article.published_at));
+    const dayGroup = dayGroups[publishDay];
+    dayGroup.group.push(article);
+    dayGroup.positiveReactionsSum += article.positive_reactions_count;
+  });
+};
+
+const analyzeDayGroups = (dayGroups: DayGroup[]) => {
+  // * Find positiveReactionsAverage for each day
+  dayGroups.forEach((dayGroup) => {
+    dayGroup.positiveReactionsAverage =
+      dayGroup.positiveReactionsSum / dayGroup.group.length;
+  });
+};
+
+const getMostPublishedDay = (dayGroups: DayGroup[]) =>
+  [...dayGroups].sort((a, b) => {
+    if (a.group.length > b.group.length) {
+      return -1;
+    } else {
+      return 1;
+    }
+  })[0].day;
+
+const getMostSuccessfulDay = (dayGroups: DayGroup[]) =>
+  [...dayGroups].sort((a, b) => {
+    if (a.positiveReactionsAverage > b.positiveReactionsAverage) {
+      return -1;
+    } else {
+      return 1;
+    }
+  })[0].day;
+
 const main = async (): Promise<void> => {
   try {
     const startOfYearDate = startOfYear(Date.now());
@@ -22,100 +80,74 @@ const main = async (): Promise<void> => {
       )}.`
     );
 
-    fetch(
-      `https://dev.to/api/articles?top=${daysSinceStartOfYear}&per_page=100`,
+    const data = await getArticleData(daysSinceStartOfYear);
+
+    if (!data) {
+      throw new Error("No data!");
+    }
+
+    const dayGroups: DayGroup[] = [
       {
-        method: "GET",
-      }
-    )
-      .then((response) => response.json())
-      .then((data: Article[]) => {
-        const dayGroups: DayGroup[] = [
-          {
-            day: "Sunday",
-            group: [],
-            positiveReactionsAverage: 0,
-            positiveReactionsSum: 0,
-          },
-          {
-            day: "Monday",
-            group: [],
-            positiveReactionsAverage: 0,
-            positiveReactionsSum: 0,
-          },
-          {
-            day: "Tuesday",
-            group: [],
-            positiveReactionsAverage: 0,
-            positiveReactionsSum: 0,
-          },
-          {
-            day: "Wednesday",
-            group: [],
-            positiveReactionsAverage: 0,
-            positiveReactionsSum: 0,
-          },
-          {
-            day: "Thursday",
-            group: [],
-            positiveReactionsAverage: 0,
-            positiveReactionsSum: 0,
-          },
-          {
-            day: "Friday",
-            group: [],
-            positiveReactionsAverage: 0,
-            positiveReactionsSum: 0,
-          },
-          {
-            day: "Saturday",
-            group: [],
-            positiveReactionsAverage: 0,
-            positiveReactionsSum: 0,
-          },
-        ];
+        day: "Sunday",
+        group: [],
+        positiveReactionsAverage: 0,
+        positiveReactionsSum: 0,
+      },
+      {
+        day: "Monday",
+        group: [],
+        positiveReactionsAverage: 0,
+        positiveReactionsSum: 0,
+      },
+      {
+        day: "Tuesday",
+        group: [],
+        positiveReactionsAverage: 0,
+        positiveReactionsSum: 0,
+      },
+      {
+        day: "Wednesday",
+        group: [],
+        positiveReactionsAverage: 0,
+        positiveReactionsSum: 0,
+      },
+      {
+        day: "Thursday",
+        group: [],
+        positiveReactionsAverage: 0,
+        positiveReactionsSum: 0,
+      },
+      {
+        day: "Friday",
+        group: [],
+        positiveReactionsAverage: 0,
+        positiveReactionsSum: 0,
+      },
+      {
+        day: "Saturday",
+        group: [],
+        positiveReactionsAverage: 0,
+        positiveReactionsSum: 0,
+      },
+    ];
 
-        // * Sort into days of the week
-        // * Add sum of `postitve_reactions_count`
-        data.forEach((datum) => {
-          // * Number 0 - 6, 0 being Sunday
-          const publishDay = getDay(new Date(datum.published_at));
-          const dayGroup = dayGroups[publishDay];
-          dayGroup.group.push(datum);
-          dayGroup.positiveReactionsSum += datum.positive_reactions_count;
-        });
+    analyzeArticles(data, dayGroups);
 
-        // * Find positiveReactionsAverage for each day
-        dayGroups.forEach((dayGroup) => {
-          dayGroup.positiveReactionsAverage =
-            dayGroup.positiveReactionsSum / dayGroup.group.length;
-        });
+    analyzeDayGroups(dayGroups);
 
-        // * Most published day of week
-        const mostPublishedDay = [...dayGroups].sort((a, b) => {
-          if (a.group.length > b.group.length) {
-            return -1;
-          } else {
-            return 1;
-          }
-        })[0].day;
-        console.log(
-          `Of the top 100 posts in that time, most are published on a ${mostPublishedDay}.`
-        );
+    // * Most published day of week
+    const mostPublishedDay = getMostPublishedDay(dayGroups);
+    console.log(
+      `Of the top 100 posts in that time, most are published on a ${mostPublishedDay}.`
+    );
 
-        // * Most successful day of the week
-        const mostSuccessfulDay = [...dayGroups].sort((a, b) => {
-          if (a.positiveReactionsAverage > b.positiveReactionsAverage) {
-            return -1;
-          } else {
-            return 1;
-          }
-        })[0].day;
-        console.log(
-          `The most successful day to publish a post is on ${mostSuccessfulDay}.`
-        );
-      })
-      .catch((error) => console.error(error));
+    // * Most successful day of the week
+    const mostSuccessfulDay = getMostSuccessfulDay(dayGroups);
+    console.log(
+      `${
+        mostPublishedDay !== mostSuccessfulDay ? "However, t" : "T"
+      }he most successful day to publish a post is on ${mostSuccessfulDay}.`
+    );
   } catch (error) {
     console.error(error);
   }
