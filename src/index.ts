aae58c6a6314a0860acffd488139af74c0f938d8
alexlee-dev <alex@alexlee.dev>
2020-12-08 09:11:37 -0800

    🧹 Refactor - Cleanup + Refactor Functionality #8 - Use Chalk

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import chalk from "chalk";
+import differenceInDays from "date-fns/differenceInDays";
 import format from "date-fns/format";
 import startOfYear from "date-fns/startOfYear";
-import differenceInDays from "date-fns/differenceInDays";
 
 import {
   analyzeArticles,
@@ -23,10 +24,9 @@ const main = async (): Promise<void> => {
       startOfYear(Date.now())
     );
     console.log(
-      `It's been ${daysSinceStartOfYear} days since ${format(
-        startOfYearDate,
-        "PPP"
-      )}.`
+      `It's been ${chalk.cyanBright.bold(
+        daysSinceStartOfYear
+      )} days since ${format(startOfYearDate, "PPP")}.`
     );
 
     const data = await getArticleData(daysSinceStartOfYear);
@@ -44,7 +44,11 @@ const main = async (): Promise<void> => {
     // * Most published day of week
     const mostPublishedDay = getMostPublishedDay(dayGroups);
     console.log(
-      `Of the top 1,000 articles in that time, most are published on a ${mostPublishedDay}.`
+      `Of the top ${chalk.cyanBright.bold(
+        "1,000"
+      )} articles in that time, most are published on a ${chalk.cyanBright.bold(
+        mostPublishedDay
+      )}.`
     );
 
     // * Most successful day of the week
@@ -52,7 +56,9 @@ const main = async (): Promise<void> => {
     console.log(
       `${
         mostPublishedDay !== mostSuccessfulDay ? "However, t" : "T"
-      }he most successful day to publish an article is on ${mostSuccessfulDay}.`
+      }he most successful day to publish an article is on ${chalk.cyanBright.bold(
+        mostSuccessfulDay
+      )}.`
     );
 
     // ? "Most successful hour throughout the week" vs "Most successful hour of most successful day"
@@ -61,14 +67,18 @@ const main = async (): Promise<void> => {
       dayGroups
     );
     console.log(
-      `The most successful hour of the most successful day is ${mostSuccessfulHourOfMostSuccessfulDay}.`
+      `The most successful hour of the most successful day is ${chalk.cyanBright.bold(
+        mostSuccessfulHourOfMostSuccessfulDay
+      )}.`
     );
 
     const mostSuccessfulHourThroughoutTheWeek = getMostSuccessfulHourThroughoutTheWeek(
       dayGroups
     );
     console.log(
-      `The most successful hour throughout the week is ${mostSuccessfulHourThroughoutTheWeek}.`
+      `The most successful hour throughout the week is ${chalk.cyanBright.bold(
+        mostSuccessfulHourThroughoutTheWeek
+      )}.`
     );
   } catch (error) {
     console.error(error);
